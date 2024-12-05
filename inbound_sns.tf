91ac85f3660a7fd94ef6a85c449fb0e0a223c202
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-12-05 17:04:18 -0500

    Update inbound_sns.tf

--- inbound_sns.tf
+++ inbound_sns.tf
@@ -33,3 +33,30 @@ resource "aws_sns_topic_subscription" "experian_inbound_email" {
 #     count.index
 #   )
 # }
+
+##########################
+## SNS Topic Policy
+##########################
+
+resource "aws_sns_topic_policy" "sns_topic_policy" {
+  arn = aws_sns_topic.experian_inbound_sns.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action    = "SNS:Publish"
+        Resource  = aws_sns_topic.experian_inbound_sns.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn": "arn:aws:s3:::inbound-astra-files"
+          }
+        }
+      }
+    ]
+  })
+}
